aff0f549c7cf1da474f44dcd7159ec187b7685b1
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2021-02-27 02:18:31 +0700

    array in state

--- part1/int1/src/index.js
+++ part1/int1/src/index.js
@@ -2,36 +2,31 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  // const [left, setLeft] = useState(0)
-  // const [right, setRight] = useState(0)
-  const [clicks, setClicks] = useState({
-    left: 0, right:0
-  })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
   const handleLeft = () => {
-    setClicks({
-      ...clicks,
-      left: clicks.left + 1,
-    })
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
   const handleRight = () => {
-    setClicks({
-      ...clicks,
-      right: clicks.right + 1
-    })
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      {clicks.left}
+      {left}
       <button onClick={handleLeft}>
         left
       </button>
       <button onClick={handleRight}>
         right
       </button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
