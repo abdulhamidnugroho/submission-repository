cae641140609934b2c19f8da4e12a8509ecb881f
hamid_n <hamid@oninyon.com>
2021-03-08 15:14:34 +0700

    phonebook step1

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -3,13 +3,13 @@ import Course from './components/Course'
 
 const App = ({ course }) => {
 	return (
-        <div>
-            <h1>Web development curriculum</h1>
-            {course.map((course) => 
-                <Course course={course} />
-            )}
-        </div>
-    )
+    <div>
+        <h1>Web development curriculum</h1>
+        {course.map((course) => 
+            <Course course={course} />
+        )}
+    </div>
+  )
 }
 
 export default App
